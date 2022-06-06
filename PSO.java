d4e1f6ce2f6f5f1230b5a7fd3aa652663e16ddcf
Rafael Camacho Roldan <rafacr_@hotmail.com>
2022-06-05 21:54:35 -0500

    Se organiza el main por pasos y se agregan las iteraciones

--- PSO.java
+++ PSO.java
@@ -11,23 +11,33 @@ class PSO{
         DecimalFormat df = new DecimalFormat("#.###");
         Methods m = new Methods();
         DataSet ds = new DataSet();
+        int t=0;
 
-        //STEP 1 
-        ds.setX_values(m.initPopulation());
-        ds.setVelocity(m.initVelocity());
-        ds.setX_values(m.initPosition( ds.getX_values(), ds.getVelocity()) );
-        ds.setpBest(m.initPBest(ds.getX_values()));
-            System.out.println("\nPositions and velocity have been initialized");
-        //STEP 2
-        ds.setFitnessFuntion(m.calculateFitnessFuntion(ds.getX_values()));
-        ds.setMinFitnessValue(m.fitnessValue(ds.getFitnessFuntion()));
-        ds.setgBest(m.calculateGlobalBest(ds.getFitnessFuntion(), ds.getMinFitnessValue(),ds.getX_values()));
-            
-        System.out.println("\nBest fitness value: "+df.format(ds.getMinFitnessValue()));
-        System.out.println("\nGlobal best values: ");
-            m.showArray(ds.getgBest());
-
-        ds.setVelocity(m.updateVelocity(ds.getVelocity(), ds.getX_values(), ds.getpBest(), ds.getgBest()));
-        ds.setX_values(m.updatePosition(ds.getVelocity(), ds.getX_values()));
+        do{
+            if(t==0){
+                //STEP 1 
+                    ds.setX_values(m.initPopulation());
+                    ds.setVelocity(m.initVelocity());
+                    ds.setX_values(m.initPosition( ds.getX_values(), ds.getVelocity()) );
+                    ds.setpBest(m.initPBest(ds.getX_values()));
+                //STEP 2
+                    ds.setFitnessFuntion(m.calculateFitnessFuntion(ds.getX_values()));
+                    ds.setMinFitnessValue(m.fitnessValue(ds.getFitnessFuntion()));
+                    ds.setgBest(m.calculateGlobalBest(ds.getFitnessFuntion(), ds.getMinFitnessValue(),ds.getX_values()));
+            }else{
+                //STEP 3
+                    ds.setVelocity(m.updateVelocity(ds.getVelocity(), ds.getX_values(), ds.getpBest(), ds.getgBest()));
+                    ds.setX_values(m.updatePosition(ds.getVelocity(), ds.getX_values()));
+                //STEP 4
+                    ds.setMinFitnessValue(m.updateFitnessValue(ds.getMinFitnessValue(), m.calculateFitnessFuntion(ds.getX_values())));
+                    ds.setgBest(m.calculateGlobalBest(m.calculateFitnessFuntion(ds.getX_values()), ds.getMinFitnessValue(), ds.getX_values())); 
+                    ds.setpBest(m.updatePBest(ds.getX_values(), ds.getpBest(), ds.getFitnessFuntion(), m.calculateFitnessFuntion(ds.getX_values())));
+            }
+                //STEP 5
+                t++;
+                //STEP 6
+                System.out.println("\nBest particle: "); m.showArray(ds.getgBest());
+                System.out.println("Global best: "+df.format(ds.getMinFitnessValue()));
+        }while(t<m.iterations);
     }
 }
